1143d6fc42b20572d454c0eb86c75b8aa87b84f0
unknown <fc56175@DUBGTSWA256269.eur.nsroot.net>
2014-04-02 14:11:45 +0100

    Data server with local .json files

---
+++ data-server/server.js
@@ -0,0 +1,280 @@
+var http = require('http');
+var https = require('https');
+var express= require("express");
+var fs = require ("fs"), json;
+var app = express();
+var url = require('url');
+var util = require ('util');
+
+
+// simple logger
+app.use(function(req, res, next){
+  console.log('%s %s', req.method, req.url);
+  next();
+});
+
+//app.use(express.logger());
+
+//for POSTS
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+
+//start server
+var server = app.listen(8080, function() {
+    //app.writeHead(200, {'Content-Type': 'text/plain'}); 
+    console.log('Listening on port %d', server.address().port);
+});
+
+// app.use(function(req, res, next){
+  // res.send(404, 'Sorry cant find that!');
+// });
+
+app.param(function(name, fn){
+  if (fn instanceof RegExp) {
+    return function(req, res, next, val){
+      var captures;
+      if (captures = fn.exec(String(val))) {
+        req.params[name] = captures;
+        next();
+      } else {
+        next('route');
+      }
+    }
+  }
+});
+
+//set up params in a URL
+app.param('major', /^\d+$/);
+app.param('minor', /^\d+$/);
+
+//redirect each URL
+app.get('/:major/:minor', function(req,res,next){
+  var major = req.params.major;
+  var minor = req.params.minor;
+  // console.log('major is' + major);
+  // console.log('minor is' + minor);
+  var beacons = getJson('beacons.json');
+ 
+ //checks if major and minor are part of the url + redirects
+  for(var i=0; i<beacons['beacons'].length; i++){
+    if (major == beacons['beacons'][i].major && minor == beacons['beacons'][i].minor){
+    // if (major == beacons['beacons'][i].major){
+      if(!req.secure){
+        res.redirect(301, beacons['beacons'][i].uri);
+      }
+    }
+  }
+  
+  next()
+  
+});
+
+//POST for adding beacons to system
+app.post ("/beacons/new", function (req, res) {
+  
+  var uuid = req.body.uuid;
+  var major = req.body.major;
+  var minor = req.body.minor;
+  var colour = req.body.colour;
+  var room = req.body.room;
+  var uri = req.body.uri;
+  var jBeacons = getJson('beacons.json')
+  
+  //add POST data to new object
+  var jObject = { "uuid": uuid, 'major':major, 'minor':minor, 'colour': colour, 'room':room, 'uri':uri};
+  
+  //add object to array
+  var pos = jBeacons['beacons'].length;
+  jBeacons['beacons'][pos]= jObject;
+  var strBeacons = JSON.stringify(jBeacons, null, 4);
+    
+  //write to .json file
+    fs.writeFile("beacons.json", strBeacons, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved.");
+        }
+    }); 
+  
+});
+
+//test page for beacon post
+app.get('/beacon/new', function(req, res){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Beacons<br>');
+    res.write('<form method="POST" action="/beacons/new">');
+    res.write('UUID<input type="text" name="uuid"><br>');
+    res.write('Major<input type="text" name="major"><br>');
+    res.write('Mnor<input type="text" name="minor"><br>');
+    res.write('Colour<input type="text" name="colour"><br>');
+    res.write('Room<input type="text" name="room"><br>');
+    res.write('URI<input type="text" name="uri"><br>');
+    res.write('<input type="submit" name="beacon" value="Submit">');
+    res.write('</form>');
+    res.end();
+
+});
+
+//retrieves all beacons
+app.get('/beacon', function(req, res) {
+  var beacons = getJson('beacons.json');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(JSON.stringify(beacons));
+  res.end();
+});
+
+app.post('/rooms/:room/booking', function(req, res) {
+
+  //POST data
+  var room = req.params.room;
+  // var room = 'solas';
+  var uri = req.body.uri;
+  var date = req.body.date;
+  var startTime = req.body.startTime;
+  var endTime = req.body.endTime;
+  var meetingName = req.body.meetingName;
+  var users = req.body.users;
+
+  var jBookings = getJson('bookings.json');
+  
+  for (var i = 0; i< jBookings['rooms'].length; i++) {
+     if (room == jBookings['rooms'][i].roomname){
+     //gets the booking data for the room
+      var newBooking = jBookings['rooms'][i];
+      //creates and adds new object to the booking array
+      var jObject = {"URI": uri, 'date':date, 'StartT':startTime, 'EndT': endTime, 'MeetingName':meetingName, 'Users':users};
+      var pos = newBooking['bookings'].length;
+      newBooking['bookings'][pos]= jObject;
+      //converts the while booking to a string for saving
+      var strBookings = JSON.stringify(jBookings, null, 4);
+      }
+  }
+
+  //write to .json file
+    fs.writeFile("bookings.json", strBookings, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved.");
+        }
+    }); 
+
+});
+  
+//for testing post
+app.get('/rooms/:room/booking', function(req, res){
+  var myroom = req.params.room;
+  //var beacons = getJson('beacons.json');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Booking<br>');
+    res.write('<form method="POST" action="/rooms/solas/booking">');
+    res.write('uri<input type="text" name="uri"><br>');
+    res.write('date<input type="text" name="date"><br>');
+    res.write('starttime<input type="text" name="startTime"><br>');
+    res.write('endtime<input type="text" name="endTime"><br>');
+    res.write('meetingname<input type="text" name="meetingName"><br>');
+    res.write('users<input type="text" name="users"><br>');
+    res.write('<input type="submit" name="booking" value="Submit">');
+    res.write('</form>');
+    res.end();
+
+});
+
+//used for storing room information before db is set up
+app.get('/rooms/:room', function(req,res){
+  //var myroom = url.parse(req.url).pathname.split('/')[2];
+  var myroom = req.params.room;
+  var bookings = getBooking(myroom);
+  var roomDetails = getRoomDetails(myroom);
+  res.send(roomDetails + bookings);
+    
+});
+
+// //used for storing room information before db is set up
+// app.get('/rooms/solas', function(req,res){
+  // var myroom = url.parse(req.url).pathname.split('/')[2];
+  // var bookings = getBooking(myroom);
+  // var roomDetails = getRoomDetails(myroom);
+  // res.send(roomDetails + bookings);
+    
+// });
+
+// //used for storing room information before db is set up
+// app.get('/rooms/nova', function(req,res){
+  // var myroom = url.parse(req.url).pathname.split('/')[2]
+  // var bookings = getBooking(myroom);
+  // var roomDetails = getRoomDetails(myroom);
+  // res.send(roomDetails + bookings);
+    
+// });
+
+// //used for storing room information before db is set up
+// app.get('/rooms/scotia', function(req,res){
+  // var myroom =url.parse(req.url).pathname.split('/')[2]
+  // var booking = getBooking(myroom);
+  // var roomDetails = getRoomDetails(myroom);
+  // res.send(roomDetails + booking);
+    
+// });
+
+//Parses .json file
+function readJsonFileSync(filepath, encoding){
+  if (typeof (encoding) == 'undefined'){
+    encoding = 'utf8';
+  }
+  var file = fs.readFileSync(filepath, encoding);
+  return JSON.parse(file);
+}
+
+function getJson(file){
+  var filepath = __dirname + '/' + file;
+  return readJsonFileSync(filepath);
+}
+
+// //returns schedule from schd.json
+// function getSchedule(room){
+    // var jSchedule = getJson('schd.json');
+    // for (var i = 0; i<jSchedule['schedule'].length;i++){
+      // if (room == jSchedule['schedule'][i].room){
+        // var strSchedule = JSON.stringify(jSchedule['schedule'][i]);
+      // }
+  // }
+  // return strSchedule;
+// }
+
+//returns schedule from desc.json
+function getRoomDetails(room){
+  var jRoom = getJson('desc.json');
+  for (var i = 0; i< jRoom['rooms'].length; i++) {
+      if (room == jRoom['rooms'][i].name){
+        var strRoom = JSON.stringify(jRoom['rooms'][i]);
+      }
+  } 
+  return strRoom;
+}
+
+function getBooking(room){
+  var jBookings = getJson('bookings.json');
+  for (var i = 0; i< jBookings['rooms'].length; i++) {
+      if (room == jBookings['rooms'][i].roomname){
+        var strBookings = JSON.stringify(jBookings['rooms'][i]);
+      }
+  } 
+  return strBookings;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
